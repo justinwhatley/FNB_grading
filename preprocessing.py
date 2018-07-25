7d429a9d4bd256c7c60c8d3a2271d26830ce712e
Justin Whatley <justinwhatley5@gmail.com>
2018-07-25 11:31:22 -0400

    Added parsing to aggregate all MG2 and MG3 data from the different datasets and split these into multiple images to increase the number of training samples

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,6 @@ import os
 from PIL import Image
 
 def tif_to_jpg(path):
-
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             print(os.path.join(root, name))
@@ -54,11 +53,31 @@ def rename_with_a_b_style(path, magnification):
                 print('Source: ' + source)
                 print('Changed to: ' + destination)
 
-# def pair_magnifications():
-#     """
-#     """
-#     pass
+def refactor_initial_datasets():
+    """
+    Only necessary when files have not yet been corrected
+    """
 
+    """ MAC dropbox paths """                
+    
+    # Change tif to jpg
+    path = '/Users/justinwhatley/Dropbox/FevensLab/FNAB_raw/dataset_3/G2'
+    tif_to_jpg(path)
+    path = '/Users/justinwhatley/Dropbox/FevensLab/FNAB_raw/dataset_3/G3'
+    tif_to_jpg(path)
+
+    # Change 100x, 400x style to standard a and b
+    path = '/Users/justinwhatley/Dropbox/FevensLab/FNAB_raw/dataset_1/MG2/100x'
+    rename_with_a_b_style(path, '100x')
+
+    path = '/Users/justinwhatley/Dropbox/FevensLab/FNAB_raw/dataset_1/MG2/400x'
+    rename_with_a_b_style(path, '400x')
+
+    path = '/Users/justinwhatley/Dropbox/FevensLab/FNAB_raw/dataset_1/MG3/100x'
+    rename_with_a_b_style(path, '100x')
+
+    path = '/Users/justinwhatley/Dropbox/FevensLab/FNAB_raw/dataset_1/MG3/400x'
+    rename_with_a_b_style(path, '400x')
 
 def crop(infile, height, width):
     """
@@ -74,7 +93,8 @@ def crop(infile, height, width):
 
 def split_images(input_path, output_path, height, width):
     """
-    Splits all images in the 
+    Splits all images in the directory to many images of specific height and width. These will be stored
+    in a the output_path
     """
     start_num = 1
     
@@ -82,17 +102,24 @@ def split_images(input_path, output_path, height, width):
         # Crops all files in the directory
         for name in files:
             # Skips files that have already been cropped
-            if 'part' in name:
+            extension = os.path.splitext(os.path.join(root, name))[1].lower().strip() 
+            print(extension)
+            if 'part' in name: 
+                print('failed load')
                 continue
 
             infile = os.path.join(root,name)
             print(infile)
-            for k, piece in enumerate(crop(infile,height,width), start_num):
-                img = Image.new('RGB', (height,width), 255)
-                img.paste(piece)
-                new_name = name.split('.')[0] + "_part%s.png" % k
-                output = os.path.join(output_path, new_name)
-                img.save(output)
+            try: 
+                for k, piece in enumerate(crop(infile,height,width), start_num):
+                    img = Image.new('RGB', (height,width), 255)
+                    img.paste(piece)
+                    # TODO change output to jpg
+                    new_name = name.split('.')[0] + "_part%s.png" % k
+                    output = os.path.join(output_path, new_name)
+                    img.save(output)
+            except Exception as e:
+                print(e)
 
 def mkdir(directory):
     """
@@ -108,12 +135,20 @@ def get_subdirectories(directory_path):
     return [os.path.join(directory_path, name) for name in os.listdir(directory_path)
             if os.path.isdir(os.path.join(directory_path, name))]
 
-def prepare_datasets(path, height, width):
-    """ 
-    Prepares training and validation sets
-    """
-    print('Loading path: ' + path)
+def bin_files(number_of_bins, files, separate_by_patient = True):
+    bins = []
 
+    return bins
+
+def get_approximate_ratio(fold = .2):
+    """
+    Separates the files randomly into different directories while ensuring individual patients do not fall into 
+    both the training and validation sets
+    """
+    number_of_bins = 1 / fold
+    pass
+
+def create_training_and_validation_dir(path):
     # Iterates through image datasets
     training_set = 'train'
     training_directory = os.path.join(path, training_set)
@@ -123,24 +158,47 @@ def prepare_datasets(path, height, width):
     validation_directory = os.path.join(validation_set)
     mkdir(validation_directory)
 
+def prepare_datasets(path, height, width):
+    """ 
+    Prepares training and validation sets
+    """
+    print('Loading path: ' + path)
+
+    # Initializes the preprocessed directory
+    preprocessed_data = 'preprocessed'
+    preprocessed_directory = os.path.join(path, preprocessed_data)
+    mkdir(preprocessed_directory)
+
     # Gets the file paths for different classes
+    MG2 = []
+    MG3 = []
     dataset_subdirs = get_subdirectories(path) 
     for dataset_subdir in dataset_subdirs:
-        get_subdirectories
-        class_dir = dataset_subdir.split('/')[-1]
-        print(class_dir)                                                                        
+        if dataset_subdir.split('/')[-1] == preprocessed_data:
+            continue
+        class_dirs = get_subdirectories(dataset_subdir)
+        for class_dir in class_dirs:
+            if class_dir.split('/')[-1] == 'MG2':
+                MG2.append(class_dir)
+            if class_dir.split('/')[-1] == 'MG3':
+                MG3.append(class_dir)
 
-    # # Finds the nested files (e.g., )
-    # for root, dirs, files in os.walk(path, topdown=False):
-        
-    #     for class_dir in dirs: 
-    #         class_path = os.path.join(root, class_dir) 
-    #         print
-    #         print(class_path)
-    #         for r, d, f in os.walk(class_path, topdown=False):
-    #             for fi in f:
-    #                 print(fi)
-    #         # split_images(path, training_set, height, width)
+    # TODO consider the change in pixel density and how this might change the interpretation by the model when not put to the same scale
+    # Splits the images and distributes the split image files into preprocessed MG2 and MG3 directories      
+    
+    MG2_directory = os.path.join(preprocessed_directory, 'MG2') 
+    mkdir(MG2_directory)
+    for dir in MG2:
+        split_images(dir, MG2_directory, height, width)  
+    
+    MG3_directory = os.path.join(preprocessed_directory, 'MG3') 
+    mkdir(MG3_directory)
+    for dir in MG3:
+        split_images(dir, MG3_directory, height, width)  
+ 
+    # create_training_and_validation_dir(preprocessed_directory)
+
+
 
 def concat_channels():
     " TODO https://stackoverflow.com/questions/43196636/how-to-concatenate-two-layers-in-keras"
@@ -163,26 +221,13 @@ def concat_channels():
     model.compile(optimizer=ada_grad, loss='binary_crossentropy',
                 metrics=['accuracy'])
 
-test_path = '/home/justin/Dropbox/Fevens Lab/FNAB_raw/'
-height, width = 224, 224
+if __name__ == "__main__":
+    # Linux path
+    # test_path = '/home/justin/Dropbox/FevensLab/FNAB_raw/'
+    
+    # Mac path
+    test_path = '/Users/justinwhatley/Dropbox/FevensLab/FNAB_raw'
 
-prepare_datasets(test_path, height, width)
+    height, width = 224, 224
 
-""" MAC dropbox paths """                
-# path = os.getcwd()
-# path = '/Users/justinwhatley/Dropbox/Fevens Lab/FNAB_raw/dataset_3/G2'
-# path = '/Users/justinwhatley/Dropbox/Fevens Lab/FNAB_raw/dataset_3/G3'
-# tif_to_jpg(path)
-
-# path = '/Users/justinwhatley/Dropbox/Fevens Lab/FNAB_raw/dataset_1/MG2/100x'
-# rename_with_a_b_style(path, '100x')
-
-# path = '/Users/justinwhatley/Dropbox/Fevens Lab/FNAB_raw/dataset_1/MG2/400x'
-# rename_with_a_b_style(path, '400x')
-
-# path = '/Users/justinwhatley/Dropbox/Fevens Lab/FNAB_raw/dataset_1/MG3/100x'
-# rename_with_a_b_style(path, '100x')
-
-# path = '/Users/justinwhatley/Dropbox/Fevens Lab/FNAB_raw/dataset_1/MG3/400x'
-# rename_with_a_b_style(path, '400x')
-
+    prepare_datasets(test_path, height, width)
